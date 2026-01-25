6c4ea404330e1ec35a06fcfaa1b021d38fa9f211
Xavier <61653230+MightyBrushwagg@users.noreply.github.com>
2026-01-25 00:23:28 +0000

    pid values

--- controllers.py
+++ controllers.py
@@ -32,16 +32,16 @@ class PIDController(Controller):
         x, x_dot, theta, theta_dot = state
         
         # Position control -> desired position
-        error_x = target_pos[0] - x 
+        error_x = - target_pos[0] + x 
         self.integral_x += error_x * self.dt
-        derivative_x = (0 - x_dot ) / self.dt
+        derivative_x = (0 - x_dot )
         
         force_x = (self.kp_x * error_x) + (self.ki_x * self.integral_x) + (self.kd_x * derivative_x)
         
         # Angle control -> force to achieve desired angle
         error_theta = target_pos[1] - theta
         self.integral_theta += error_theta * self.dt
-        derivative_theta = (0 - theta_dot) / self.dt
+        derivative_theta = (0 - theta_dot) 
         
         force_theta = (self.kp_theta * error_theta) + (self.ki_theta * self.integral_theta) + (self.kd_theta * derivative_theta)
         
